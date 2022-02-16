ef5b872b261bfe6ac6131a39662535351675633c
rrayner.tan <rayner.tan@ncs.com.sg>
2022-02-16 11:04:07 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -29,14 +29,22 @@ public class MyStringStackTest extends TestCase {
 	public void tearDown() throws Exception {
 	}
 
+//	@Test
+//	public void testPush() {
+//		String o1 = null;  
+//		MyStringStack stack = new MyStringStack();
+//		assertTrue (stack.isEmpty());
+//		stack.push (s1);
+//        stack.push (s2);
+//        assertFalse (stack.isEmpty());
+//	}
+	
 	@Test
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
